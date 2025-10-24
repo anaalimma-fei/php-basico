f1f906d8282b75521045a2b7932b37035d6e3d1a
ana paula <ana.p.ferreira26@aluno.senai.br>
2025-10-24 08:54:38 -0300

    terminado os exercicios de php

---
+++ 10a_insercao.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Clientes</title>
+</head>
+<body>
+
+<form method="post" action="">
+    <label for="Nome">Nome do Produto:</label>
+    <input type="text" name="nome" required><br>
+
+    <label for="peco">Preço:</label>
+    <input type="number" min="0.01" step="0.01" name="preco" required><br>
+
+    <button type="submit">Adicionar</button>
+</form>
+
+<?php
+    //Verifica se o formulário foi enviado
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Recebe os valores enviados pelo formulário 
+            $nome = $_POST['nome'];
+            $preco = $_POST['preco'];
+
+            // Conexão com o banco de dados
+            $servername = "127.0.0.1";
+            $username = "root";
+            $password = "Senai@118";
+            $dbname = "exercicio";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Verifica a conexão
+            if($conn->connect_error) {
+                die("Falha na conexão: " . $conn->connect_error);
+            }
+
+            //Insere o registro no banco de dados
+            $sql = "INSERT INTO  produtos  (nome, preco) VALUES ('$nome','$preco')";
+
+            if ($conn->query($sql) === TRUE){
+                echo "<p style= 'color:Darkgreen;'> Produto adicionado com sucesso</p>";
+            } else {
+                echo "<p style= 'color:Red;'> Erro ao cadastrar.</p>";
+            }
+
+            //Fechar a conexão
+            $conn->close();
+        }
+        
+    ?>
+</body>
+</html>
--- 12_atualizar.php
+++ 12_atualizar.php
@@ -1,5 +1,28 @@
 <!-- Passar id via URL -->
 <!-- http://localhost/php-basicos/12_atualizar.php?id=1-->
 
+<?php
+//Conecta ao banco de dados 
+$servername = "localhost"; 
+$username = "root";
+$password = "Senai@118";
+$dbname = "exercicio";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Verifica a conexão
+if ($conn->connect_error){
+    die ("falha a conexão: " . $conn->connect_error);
+}
+
+//Inicializa a variavel $cliente com null 
+$cliente = null;
+
+//Verificase um ID foi passado via URL para edição
+if (isset ($_GET['id'])){
+    $id = $_GET['id'];
+    $sql
+}
+?>
 
 
