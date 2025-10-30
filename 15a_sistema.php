6498f469036bd8bb19cb4510bfdb556038523864
ana paula <ana.p.ferreira26@aluno.senai.br>
2025-10-30 11:10:25 -0300

    até exemplo 15
    Códigos digitados

---
+++ 15a_sistema.php
@@ -0,0 +1,44 @@
+<?php
+//Página de login (15a_sistema.php)
+session_start();
+
+//Verifica se o formulário foi enviado 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    //Verifica se os dados são validos (usuario: admin, senha: 123)
+    if($usuario == 'admin' && $senha == '123'){
+       $_SESSION['usuario'] = $usuario; // Salva o nome do usuario na sessão 
+       header("Location: 15_restrita.php");
+       exit();
+    } else{
+        $erro = "Usuário ou senha incorretos.";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="usuario">Usuário:</label>
+        <input type="text" name="usuario" required><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" required><br>
+ 
+        <button type="submit">Entrar</button>
+        
+    </form>
+
+    <?php
+    if (isset($erro)) {
+        echo "<p style='color: red;'>$erro</p>";
+    }
+    ?>
+</body>
+</html>
